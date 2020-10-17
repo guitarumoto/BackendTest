c7a207a58c36e3adee85a81cc518a1eadce206c0
guitarumoto <guitarumoto@gmail.com>
2020-10-16 23:38:22 -0300

    criando função para listar asc e desc

---
+++ src/functions/orderBy.js
@@ -0,0 +1,24 @@
+
+module.exports = {
+    orderbyasc(result){
+        var asc = []
+        result.map(booksonarray =>{
+            asc.push(booksonarray)
+            asc.sort(function(a, b){
+                return parseFloat(a.price) - parseFloat(b.price)});
+        })
+        
+
+        return asc
+    },
+    orderbydesc(result){
+        var desc = []
+        result.map(booksonarray =>{
+            desc.push(booksonarray)
+            desc.sort(function(a, b){
+                return parseFloat(b.price) - parseFloat(a.price)});
+        })
+        
+        return desc
+    }
+}
